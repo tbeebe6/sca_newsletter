afd6de5ffe8c87482c0660d61ce809af4868da6f
Tom Beebe <tbeebe@gmail.com>
2023-07-27 16:17:26 -0400

    Moved cron into the main php file

--- sca_pull_newsletter.php
+++ sca_pull_newsletter.php
@@ -17,4 +17,20 @@ require_once(WP_PLUGIN_DIR .'/sca_pull_newsletter/classes/sca_newsletter.php');
 require_once(WP_PLUGIN_DIR .'/sca_pull_newsletter/lib/get_latest_sca.php');
 require_once(WP_PLUGIN_DIR .'/sca_pull_newsletter/lib/process_newsletter.php');
 require_once(WP_PLUGIN_DIR .'/sca_pull_newsletter/lib/sca_shortcode.php');
-require_once(WP_PLUGIN_DIR .'/sca_pull_newsletter/lib/sca_cron.php');
+
+
+# Run the check daily for the new newsletter
+register_activation_hook( __FILE__, 'sca_newsletter_activation' );
+
+add_action( 'sca_daily_event', 'sca_check_newsletter' );
+
+function sca_newsletter_activation() {
+    wp_schedule_event( time(), 'daily', 'sca_daily_event' );
+}
+
+register_deactivation_hook( __FILE__, 'sca_newsletter_deactivation' );
+
+function sca_newsletter_deactivation() {
+    wp_clear_scheduled_hook( 'sca_daily_event' );
+}
+
